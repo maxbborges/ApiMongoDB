eb2b82cbed6b356be208f1c9d834ab4644e87533
Maxwell <max15borges@gmail.com>
2021-07-10 12:40:24 -0300

    Commit Inicial

---
+++ ApiMongoDB/Controllers/InfectadoController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using ApiMongoDB.Data.Collections;
+using ApiMongoDB.Models;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+
+namespace ApiMongoDB.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class InfectadoController : ControllerBase
+    {
+        Data.MongoDB _mongoDB;
+        IMongoCollection<Infectado> _infectadosCollection;
+
+        public InfectadoController(Data.MongoDB mongoDB)
+        {
+            _mongoDB = mongoDB;
+            _infectadosCollection = _mongoDB.DB.GetCollection<Infectado>(typeof(Infectado).Name.ToLower());
+        }
+
+        [HttpPost]
+        public ActionResult SalvarInfectado([FromBody] InfectadoDto dto)
+        {
+            var infectado = new Infectado(dto.DataNascimento, dto.Sexo, dto.Latitude, dto.Longitude);
+
+            _infectadosCollection.InsertOne(infectado);
+
+            return StatusCode(201, "Infectado adicionado com sucesso");
+        }
+
+        [HttpGet]
+        public ActionResult ObterInfectados()
+        {
+            var infectados = _infectadosCollection.Find(Builders<Infectado>.Filter.Empty).ToList();
+
+            return Ok(infectados);
+        }
+
+        [HttpPut]
+        public ActionResult AtualizarInfectados([FromBody] InfectadoDto dto)
+        {
+            _infectadosCollection.UpdateOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dto.DataNascimento), Builders<Infectado>.Update.Set("sexo", dto.Sexo));
+            return Ok();
+        }
+
+        [HttpDelete("{dataNasc}")]
+        public ActionResult DeletarInfectados(DateTime dataNasc)
+        {
+            _infectadosCollection.DeleteOne(Builders<Infectado>.Filter.Where(_ => _.DataNascimento == dataNasc));
+            return Ok();
+        }
+    }
+}
